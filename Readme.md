3187280c55944c0051d2bccb58da4c2aa947b4af
saeedx302 <114274102+SaeedX302@users.noreply.github.com>
2020-09-18 13:40:00 +0000

    add function for extract activation links from cutout.pro

---
+++ Readme.md
@@ -0,0 +1,26 @@
+## How to Use the Script
+### Install Dependencies:
+You need the You'll need python-socketio, requests, and beautifulsoup4 library
+
+### Run this command in your terminal
+
+    pip install "python-socketio[client]" requests beautifulsoup4
+
+### Run the Script
+
+    python main.py
+
+## Example Output
+
+### When an email arrives from cutout.pro, you will see
+============================================================
+>NEW EMAIL RECEIVED <<
+
+> From:    service@cutout.pro
+
+> Subject: Activate your cutout.pro account [*] Detecting cutout.pro email. Extracting activation link...
+
+> ACTIVATION LINK: https://www.cutout.pro/resBackMsg...
+
+> Full Msg: https://www.fakemailgenerator.com/inbox/dayre...
+============================================================
--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import socketio
 import json
+import requests
+from bs4 import BeautifulSoup
 import sys
 import time
+import re
 
 # Configuration
 INBOX_ADDRESS = "tsuncca22@dayrep.com"
 SOCKET_URL = "https://ws.fakemailgenerator.com/"
+BASE_URL = "https://www.fakemailgenerator.com"
 
 # Initialize Socket.io client
 sio = socketio.Client()
+
+def extract_activation_link(email_id, domain, recipient):
+    """Fetches the email content and extracts the cutout.pro activation link."""
+    # The actual content is inside an iframe with a specific URL structure
+    content_url = f"{BASE_URL}/email/{domain}/{recipient}/message-{email_id}/"
+    
+    try:
+        # Add a timeout and a user-agent to look like a real browser
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(content_url, headers=headers, timeout=10)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            # Look for links containing 'cutout.pro/resBackMsg'
+            # The user specified /html/body/div[1]/div/a
+            links = soup.find_all('a', href=True)
+            for link in links:
+                href = link['href']
+                if 'cutout.pro/resBackMsg' in href:
+                    return href
+            
+            # Fallback: search for the link pattern in the raw text if soup fails
+            match = re.search(r'https://www\.cutout\.pro/resBackMsg\?type=2&token=[a-zA-Z0-9]+', response.text)
+            if match:
+                return match.group(0)
+                
+    except Exception as e:
+        print(f"[!] Error fetching email content: {e}")
+    
+    return None
 
 @sio.event
 def connect():
@@ -24,8 +61,6 @@ def disconnect():
 @sio.on("incoming_email")
 def on_incoming_email(data):
     try:
-        # In python-socketio, data is usually already parsed if it's JSON
-        # but let's be safe.
         email = data
         if isinstance(data, str):
             try:
@@ -33,37 +68,41 @@ def on_incoming_email(data):
             except:
                 pass
             
-        print("\n" + "="*50)
-        print(">>> NEW EMAIL RECEIVED <<<")
-        
-        # Display the email details
         sender = email.get('sender', 'Unknown Sender')
         subject = email.get('subject', 'No Subject')
-        time_received = email.get('timeonly', 'N/A')
         email_id = email.get('emailid', 'N/A')
         domain = email.get('domain', 'N/A')
         recipient = email.get('recipient', 'N/A')
         
+        print("\n" + "="*60)
+        print(">>> NEW EMAIL RECEIVED <<<")
         print(f"From:    {sender}")
         print(f"Subject: {subject}")
-        print(f"Time:    {time_received}")
-        print(f"Full ID: {email_id}")
         
-        # Construct the URL to read the full message
-        if email_id != 'N/A' and domain != 'N/A' and recipient != 'N/A':
-            read_url = f"https://www.fakemailgenerator.com/inbox/{domain}/{recipient}/message-{email_id}/"
-            print(f"Link:    {read_url}")
+        # Check if the email is from cutout.pro
+        if 'cutout.pro' in sender.lower() or 'cutout.pro' in subject.lower():
+            print("[*] Detecting cutout.pro email. Extracting activation link...")
+            # Wait a second for the server to process the email content
+            time.sleep(2)
+            activation_link = extract_activation_link(email_id, domain, recipient)
             
-        print("="*50 + "\n")
+            if activation_link:
+                print(f"\033[92mACTIVATION LINK: {activation_link}\033[0m")
+            else:
+                print("[!] Could not find activation link in the email body.")
+        
+        if email_id != 'N/A':
+            read_url = f"{BASE_URL}/inbox/{domain}/{recipient}/message-{email_id}/"
+            print(f"Full Message: {read_url}")
+            
+        print("="*60 + "\n")
         
     except Exception as e:
         print(f"[!] Error processing data: {e}")
-        print(f"Raw data: {data}")
 
 def main():
-    print("[*] Starting FakeMail Monitor...")
+    print("[*] Starting FakeMail Monitor with Link Extraction...")
     try:
-        # Use websocket transport for efficiency
         sio.connect(SOCKET_URL, transports=['websocket'])
         sio.wait()
     except KeyboardInterrupt:
